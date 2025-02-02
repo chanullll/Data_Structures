9ee136ab0e2f3e867fe85c4c8e7710e124f8f4f2
chanullll <chanuldilmith200@gmail.com>
2025-02-02 21:49:14 +0530

    Add files via upload

--- PriorityQueueExample.java
+++ PriorityQueueExample.java
@@ -1,9 +1,24 @@
+import java.util.PriorityQueue;
 
 public class PriorityQueueExample {
+    public static void main(String[] args) {
+        // Create a PriorityQueue (Min-Heap by default)
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Adding elements to the PriorityQueue
+        pq.add(30);
+        pq.add(10);
+        pq.add(20);
+        pq.add(5);
 
-	}
+        // Displaying the PriorityQueue
+        System.out.println("PriorityQueue: " + pq);
 
+        // Removing elements from the PriorityQueue (smallest element first)
+        System.out.println("Polled element: " + pq.poll());
+        System.out.println("PriorityQueue after poll: " + pq);
+
+        // Peeking the top element without removing
+        System.out.println("Top element: " + pq.peek());
+    }
 }
